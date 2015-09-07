bfd8ea55be49dc1959032b34d91bc9bdaf42ec8b
addischang1991 <addischang1991@livemail.tw>
2015-09-07 23:28:04 +0800

    First commit

---
+++ inc/main.h
@@ -0,0 +1,50 @@
+/**
+  ******************************************************************************
+  * @file    : main.h
+  * @author  : Chang, Wei-Chieh
+  * @version : V1.0.0
+  * @date    : 17-Augest-2015
+  * @brief   : This file contains necessary headfile for the main.c
+  ******************************************************************************
+  * @license : GNU GPL version 3 or later <http://www.gnu.org/licenses/gpl.html>.
+  *
+  * Copyright (C) 2015 Chang, Wei-Chieh
+  *
+  * This file is part of MAIN.
+  *
+  * MAIN is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * MAIN is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with MAIN. If not, see <http://www.gnu.org/licenses/>.
+  *
+  ******************************************************************************
+  */
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_spi.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_syscfg.h"
+  
+#include "stm32f429i_discovery.h"
+#include "stm32f429i_discovery_l3gd20.h"
+#include "stm32f429i_discovery_lcd.h"
+#include "stm32f429i_discovery_ioe.h"
+
+#endif
